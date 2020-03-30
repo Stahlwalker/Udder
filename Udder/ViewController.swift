325592af24cbda7c2ef81e14137bc1ede4ebbbf7
Stahlwalker <Stahl1109@gmail.com>
2020-03-30 13:45:04 -0500

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Stahl, Lucas on 3/30/20.
+//  Copyright © 2020 Stahlwalker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
